0b5f2452d45a716578951024dca5511dfd99ce7d
Daniel Ivanov <daniel.ivanov@mavigex.com>
2022-04-25 23:34:55 +0200

    Initial Commit

---
+++ Shiftez/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shiftez
+//
+//  Created by Daniel Ivanov on 25/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shiftez/ShiftezApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftezApp.swift
+//  Shiftez
+//
+//  Created by Daniel Ivanov on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ShiftezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
